89f419efab280324f530f33070b7beaff0430afe
Michael Pope <map7777@gmail.com>
2010-07-10 23:00:27 +1000

    Added irexec config Updated myth_start.sh to kill all currently run mythfrontends on this terminal before starting a new one.

--- myth_start.sh
+++ myth_start.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-mythfrontend
+MYTHEXISTS=`/bin/ps -e | grep mythfrontend | grep -v grep | awk '{print $1}'`;
+
+if [ "$MYTHEXISTS" != "" ]; then
+        for MYTHPROCESS in `/bin/ps -e | grep mythfrontend | grep -v grep | awk '{print $1}'`
+        do
+                echo $MYTHPROCESS;
+                kill -9 $MYTHPROCESS;
+        done
+fi
+
+DISPLAY=:0 mythfrontend &
